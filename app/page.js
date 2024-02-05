89b07c53dfdfe61385c26f382c4259e3b3eea72a
zainytech <zainabrasheed4142@gmail.com>
2024-02-04 20:43:01 -0800

    trying to add a new commit

--- app/page.js
+++ app/page.js
@@ -105,6 +105,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Hi trying to commit changes.
           </p>
         </a>
       </div>
