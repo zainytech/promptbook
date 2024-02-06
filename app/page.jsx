24737d9fe4802a96acf4968e04b39ad72689f13a
zainytech <zainabrasheed4142@gmail.com>
2024-02-06 13:28:48 -0800

    checking if CI/CD ipeline created or not

--- app/page.jsx
+++ app/page.jsx
@@ -11,6 +11,7 @@ const Home = () => (
     <p className="desc text-center">
       PromptBook is an open-source AI prompting tool for modern world to  discover, create and share creative prompts.
     </p>
+    <p>checking if CI/CD Pipeline created or not</p>
     {/* Feed */}
   </section>
 )
