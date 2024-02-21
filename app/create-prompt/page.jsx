c8c95b82ae92079e45ed292c54a2c086ff3cab97
zainytech <zainabrasheed4142@gmail.com>
2024-02-21 14:35:33 -0800

    successfully created the create-prompt page in which user can create a new prompt, then that prompt is going to get saved in our mongoodb using post method

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,14 +6,34 @@ import { useRouter } from 'next/navigation';
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
+    const router = useRouter();
+    const {data:session} = useSession();
   const [submitting,setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
     tag: '',
-
   });
-  const createPrompt = (e) =>{
+  const createPrompt = async (e) =>{
+    e.preventDefault();
+    setSubmitting(true);
 
+    try{
+        const response= await fetch('/api/prompt/new', {
+            method:"POST",
+            body:JSON.stringify({
+                prompt:post.prompt,
+                userId: session?.user.id,
+                tag:post.tag
+            })
+        })
+        if(response.ok){
+            router.push('/')
+        }
+    } catch(error){
+        console.log(error)
+    } finally {
+        setSubmitting(false);
+    }
   }
     return (
     <Form
