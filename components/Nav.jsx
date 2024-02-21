e01aedc0bdefbc4fb029c6b730504d0d087b8d29
zainytech <zainabrasheed4142@gmail.com>
2024-02-20 21:29:47 -0800

    profile pic from github profile is showing

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -35,7 +35,7 @@ const Nav = () => {
             </Link>
             <button type='button' onClick={signOut} className='outline_btn'>Sign Out</button>
             <Link href='/profile'>
-              <Image src="assets/images/logo.svg" height={37} width={37} className='rounded-full' alt='profile' />
+              <Image src={session?.user.image} height={37} width={37} className='rounded-full' alt='profile' />
             </Link>
           </div>
         ): (
@@ -89,34 +89,3 @@ const Nav = () => {
 }
 
 export default Nav
-
-// "use client";
-
-// import Link from "next/link";
-// import { signIn, signOut, useSession } from "next-auth/react";
-
-// export default function Nav() {
-//   const { status } = useSession();
-//   return (
-//     <div className="p-4 flex justify-between items-center shadow-md">
-//       <Link className="font-bold text-lg text-blue-700" href={"/"}>
-//         GTCoding
-//       </Link>
-//       {status === "authenticated" ? (
-//         <button
-//           onClick={() => signOut()}
-//           className="bg-slate-900 text-white px-6 py-2 rounded-md"
-//         >
-//           Sign Out
-//         </button>
-//       ) : (
-//         <button
-//           onClick={() => signIn("google")}
-//           className="bg-slate-900 text-white px-6 py-2 rounded-md"
-//         >
-//           Sign In
-//         </button>
-//       )}
-//     </div>
-//   );
-// }
