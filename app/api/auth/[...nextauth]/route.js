e4568a78205dc80b29ed92f936ad36a04c69312b
zainytech <zainabrasheed4142@gmail.com>
2024-02-20 17:41:44 -0800

    google provider not working hence used github provider

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,18 +5,21 @@ import User from '@models/user';
 import {connectToDB} from '@utils/database';
 // console.log("user",User)
 // console.log(
-//     {clientId: process.env.GOOGLE_ID,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     {clientId: process.env.GITHUB_ID,
+//     clientSecret: process.env.GITHUB_SECRET,
 // }
 // )
 // connectToDB();
 const authOptions = {
     providers : [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_ID,
+        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        // }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
         }),
-
     ],
     callbacks:{
         
