f63b93b22f6d32cf5833daa30a352e1d1400b420
zainytech <zainabrasheed4142@gmail.com>
2024-02-22 22:25:57 -0800

    codded the feed page which shows all the saved prompts in database to our homepage using get request (with interactive card design)

--- components/Form.jsx
+++ components/Form.jsx
@@ -13,7 +13,7 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
 
       <form onSubmit={handleSubmit} className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'>
         <label>
-          <span className='font-satoshi font-semibold text-base text-grey-700'>Your AI Prompt</span>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>Your AI Prompt</span>
           <textarea 
             vlaue={post.prompt} 
             onChange={(e)=> setPost({...post, prompt:e.target.value})} 
@@ -23,7 +23,7 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
           />
         </label>
         <label>
-          <span className='font-satoshi font-semibold text-base text-grey-700'>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
             Tag
             <span className='font-normal'> (#product,#webdevelopment,#idea)</span>
           </span>
@@ -36,7 +36,7 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
           />
         </label>
         <div className='flex-end mx-3 mb-5 gap-4'>
-          <Link href="/" className='text-grey-500 text-sm'>Cancel</Link>
+          <Link href="/" className='text-gray-500 text-sm'>Cancel</Link>
           <button type="submit" disabled={submitting} className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'>
           { submitting ? `${type}...`: type }
         </button>
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // images: {
+    //     domains: ['avatars.githubusercontent.com'],
+    //   }
+    //this is depracated for above next14..use remotepatterns instead of domains
+    
     images: {
-        domains: ['avatars.githubusercontent.com'],
-      }
+      remotePatterns: [
+        {
+          // protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+          // pathname: '**',
+        },
+      ],
+    },
 };
 
 export default nextConfig;
