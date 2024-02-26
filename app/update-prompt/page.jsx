9698896eeb853170fd5369941b024950b960fcc8
zainytech <zainabrasheed4142@gmail.com>
2024-02-25 16:47:40 -0800

    solving error of useSearchParams() must be wrapped in a suspense baundary

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,62 +1,12 @@
-"use client";
-import {useEffect, useState} from 'react'
-import { useRouter,useSearchParams } from 'next/navigation';
-
-import Form from '@components/Form';
+import UpdadtePrompt from './update-prompt'
+import { Suspense } from 'react';
 
 const EditPrompt = () => {
-    const router = useRouter();
-  const [submitting,setSubmitting] = useState(false);
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get('id')
+  return (
 
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  });
-
-  useEffect(()=>{
-    const getPromoptDetails = async () =>{
-        const response = await  fetch(`/api/prompt/${promptId}`);
-        const data = await response.json();
-        setPost({
-            prompt:data.prompt,
-            tag: data.tag,
-        })
-    }
-    if(promptId) getPromoptDetails();
-  },[promptId])
-
-//   const createPrompt = async (e) =>{
-//     e.preventDefault();
-//     setSubmitting(true);
-
-//     try{
-//         const response= await fetch('/api/prompt/new', {
-//             method:"POST",
-//             body:JSON.stringify({
-//                 prompt:post.prompt,
-//                 userId: session?.user.id,
-//                 tag:post.tag
-//             })
-//         })
-//         if(response.ok){
-//             router.push('/')
-//         }
-//     } catch(error){
-//         console.log(error)
-//     } finally {
-//         setSubmitting(false);
-//     }
-//   }
-    return (
-    <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={()=>{}}
-    />
+<Suspense fallback={<div>Loading...</div>}>
+  <UpdadtePrompt />
+</Suspense>
   )
 }
 
