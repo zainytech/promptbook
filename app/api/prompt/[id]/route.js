ed7ee1ed915f786b4d07f5aaee76e33cebd52ebe
zainytech <zainabrasheed4142@gmail.com>
2024-02-25 21:59:58 -0800

    delete button of prompt cards is working, now users can delete the prompt they created themselves by going to their profile page

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -37,7 +37,7 @@ export const PATCH = async (request,{params}) =>{
 export const DELETE = async(request,{params}) =>{
     try{
         await connectToDB();
-        await Prompt.findByIdAndRemove(params.id);
+        await Prompt.findByIdAndDelete(params.id);
         return new Response("Prompt deleted successfully",{status:200})
 
     } catch(error){
