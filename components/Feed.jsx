c80923804baf97fc7245b9c4474f7bb9e525c98e
zainytech <zainabrasheed4142@gmail.com>
2024-02-26 19:51:13 -0800

    users can search the prompts on homepage, the prompts containing user's entered word or tagname will be shown on homepage

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,12 +2,30 @@
 import { useState, useEffect } from 'react'
 
 import PromptCard from './PromptCard';
-const PromptCardList = ({data,handleTagClick})=>{
+
+const PromptCardList = ({data,handleTagClick,searching})=>{
+
+  const [filteredPosts, setFilteredPosts] = useState();
+
+  useEffect(()=>{
+    const filteredData = data.filter((coin) => coin.tag.toLowerCase().includes(searching.toLowerCase()) || coin.prompt.toLowerCase().includes(searching.toLowerCase()) );
+    setFilteredPosts(filteredData);
+  },[data,searching])
+
   return (
     <div className='mt-16 prompt_layout'>
-      {data.map((post)=>(
-        <PromptCard key={post._id} post={post} handleTagClick = {handleTagClick}/>
-      ))}
+      {searching == "" ? (
+          data.map((post)=>(
+            <PromptCard key={post._id} post={post} handleTagClick = {handleTagClick}/>
+          ))
+        )
+      : (
+        filteredPosts.map((post)=>(
+          <PromptCard key={post._id} post={post} handleTagClick = {handleTagClick}/>
+        ))
+      )
+      }
+      
     </div>
   )
 }
@@ -17,24 +35,28 @@ const Feed = () => {
   const [posts,setposts] = useState([]);
 
   const handleSearchChange = (e) =>{
-
+    e.preventDefault();
+    const inputValue =  e.target.value;
+    setSearchText(inputValue);
   }
+
+
   useEffect(()=>{
     const fetchPosts = async () =>{
       const response = await fetch('/api/prompt');
       const data = await response.json();
       setposts(data);
     }
-    // console.log(posts);
     fetchPosts();
   },[])
-
+console.log(posts)
   return (
     <section className='feed'>
       <form className='relative w-full flex-center'>
         <input type="text" placeholder='Search for a tag or a username' value={searchText} onChange={handleSearchChange} required className='search_input peer'/>
       </form>
-      <PromptCardList data={posts} handleTagClick = {()=>{}}/>
+
+      <PromptCardList data={posts} handleTagClick = {()=>{}} searching={searchText}/>
     </section>
   )
 }
